c6ae4fdc7137dac319e5aa09fcff1630030f5ba3
Zen Voich <zen.voich@gmail.com>
2019-05-09 03:46:01 +0300

    fork

--- plugin/index.js
+++ plugin/index.js
@@ -5,35 +5,24 @@ export default function({ types: t }) {
     constructor() {
       return {
         visitor: {
-          ImportDeclaration: {
-            exit(path, state) {
-              const givenPath = path.node.source.value;
-              let reference = state && state.file && state.file.opts.filename;
-              const extensions = state && state.opts && state.opts.extensions;
-
-              if (BabelInlineImportHelper.shouldBeInlined(givenPath, extensions)) {
-                if (path.node.specifiers.length > 1) {
-                  throw new Error(`Destructuring inlined import is not allowed. Check the import statement for '${givenPath}'`);
-                }
-
-                const id = path.node.specifiers[0].local.name;
-                const content = BabelInlineImportHelper.getContents(givenPath, reference);
-                const variable = t.variableDeclarator(t.identifier(id), t.stringLiteral(content));
-
-                path.replaceWith({
-                  type: 'VariableDeclaration',
-                  kind: 'const',
-                  declarations: [variable],
-                  leadingComments: [
-                    {
-                      type: 'CommentBlock',
-                      value: ` babel-plugin-inline-import '${givenPath}' `
-                    }
-                  ]
-                });
+          CallExpression: {
+            exit: function exit(path, state) {
+              if (path.node.callee.type !== 'Import') {
+                return;
               }
-            }
-          }
+              var givenPath = path.node.arguments[0].value;
+              var reference = state && state.file && state.file.opts.filename;
+              let extensions = state && state.opts && state.opts.extensions;
+
+              if (!BabelInlineImportHelper.shouldBeInlined(givenPath, extensions)) {
+                return;
+              }
+
+              var content = BabelInlineImportHelper.getContents(givenPath, reference);
+
+              path.replaceWith(t.expressionStatement(t.stringLiteral(content)))
+            },
+          },
         }
       };
     }
